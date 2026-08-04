5b1f9ab616c59e8f5e83fee776b2b23fe5aec481
mhmad-diab <mhmadiab17@gmail.com>
2026-08-04 13:15:27 +0300

    added XML Parsesr functions

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import { readCSVFile , writeCSVFile } from "./utility/CSVParser";
+import { writeXMLFile, readXMLFile } from "./utility/XMLParser";
 import path from 'path'
 
+
 const filePath = path.resolve(process.cwd(), "src/data/people-100.csv");
-const outputPath = path.resolve(process.cwd(), "src/data/output.csv");
+// const outputPath = path.resolve(process.cwd(), "src/data/output.csv");
 
 // async function main() {
 //     try {
@@ -20,29 +22,77 @@ const outputPath = path.resolve(process.cwd(), "src/data/output.csv");
 
 
 
+// async function main() {
+//     try {
+//         const data: string[][] = [
+//             ["Name", "Age", "City"],
+//             ["John", "25", "Beirut"],
+//             ["Sara", "30", "Tripoli"],
+//             ["Ali", "28", "Saida"]
+//         ];
+
+//         // Write CSV file
+//         await writeCSVFile(outputPath, data);
+
+//         console.log("CSV file written successfully!");
+
+//         // Verify by reading it back
+//         const result = await readCSVFile(outputPath);
+
+//         console.log("CSV content:");
+//         result.forEach(row => console.log(row));
+
+//     } catch (error) {
+//         console.error(error);
+//     }
+// }
+
+// main();
+
+
+const inputPath = path.resolve(
+    process.cwd(),
+    "src/data/toy orders.xml"
+);
+
+const outputPath = path.resolve(
+    process.cwd(),
+    "src/data/output.xml"
+);
+
+
 async function main() {
+
     try {
-        const data: string[][] = [
-            ["Name", "Age", "City"],
-            ["John", "25", "Beirut"],
-            ["Sara", "30", "Tripoli"],
-            ["Ali", "28", "Saida"]
-        ];
 
-        // Write CSV file
-        await writeCSVFile(outputPath, data);
+        // Reading XML
+        const xmlData = await readXMLFile(inputPath);
 
-        console.log("CSV file written successfully!");
+        console.log("XML Content:");
+        console.log(JSON.stringify(xmlData, null, 2));
 
-        // Verify by reading it back
-        const result = await readCSVFile(outputPath);
 
-        console.log("CSV content:");
-        result.forEach(row => console.log(row));
+        // Writing XML
 
-    } catch (error) {
+        const data = {
+            person: {
+                name: "Ahmad",
+                age: 25,
+                city: "Beirut"
+            }
+        };
+
+
+        await writeXMLFile(outputPath, data);
+
+        console.log("XML file created successfully");
+
+
+    } catch(error) {
         console.error(error);
     }
+
 }
+
 
 main();
